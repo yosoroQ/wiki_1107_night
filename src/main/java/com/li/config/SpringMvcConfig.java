1ea464df384149b6cd089fe029218c40bd6190fe
“yosoroQ” <1249508470@qq.com>
2023-02-16 22:54:11 +0800

    0216晚上 文档阅读数更新

--- src/main/java/com/li/config/SpringMvcConfig.java
+++ src/main/java/com/li/config/SpringMvcConfig.java
@@ -1,21 +1,29 @@
-//package com.li.config;
-//
-//import com.li.interceptor.LogInterceptor;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//import javax.annotation.Resource;
-//
-//@Configuration
-//public class SpringMvcConfig implements WebMvcConfigurer {
-//
-//    @Resource
-//    LogInterceptor logInterceptor;
-//
-//    public void addInterceptor(InterceptorRegistry registry) {
-//        registry.addInterceptor(logInterceptor)
-//                .addPathPatterns("/**").excludePathPatterns("/login");
-//    }
-//}
+package com.li.config;
+
+import com.li.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LoginInterceptor loginInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/test/**",
+                        "/redis/**",
+                        "/user/login",
+                        "/category/all",
+                        "/ebook/list",
+                        "/doc/all/**",
+                        "/doc/find-content/**"
+                );
+    }
+}
