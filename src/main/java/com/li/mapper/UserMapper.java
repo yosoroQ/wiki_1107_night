3c1af0b96b6d8700b7c9c64f1ba1fd936d2b79b1
“yosoroQ” <1249508470@qq.com>
2023-02-16 22:29:34 +0800

    0216晚上 增加退出登录功能，退出登录时，前端清除store登录信息，后端清除redis登录信息

---
+++ src/main/java/com/li/mapper/UserMapper.java
@@ -0,0 +1,31 @@
+package com.li.mapper;
+
+import com.li.entity.User;
+import com.li.entity.UserExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface UserMapper {
+    long countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExample(UserExample example);
+
+    User selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
